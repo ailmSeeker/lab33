31ffb283ca463bf60c417d77bf732066011d1c3f
Mohamed Yusuf <study@MohamedYusufs-MacBook-Pro.local>
2023-06-12 18:18:32 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+let currentUser = "Mohamed";
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = (`Welcome to Flatbook, ${currentUser.toUpperCase()}!`).toUpperCase()
+let shortGreeting = `Welcome, ${currentUser[0]}!`
+
